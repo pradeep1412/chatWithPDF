670ae6521f83c967c5c93816e661d26fff6a38f4
Pradeep Kumar P <61228637+pradeep1412@users.noreply.github.com>
2023-09-09 16:15:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -90,23 +90,23 @@ def main():
         st.session_state.conversation = get_conversation_chain(vectorstore)
         handle_userinput(user_question)
 
-    with st.sidebar:
-        st.subheader("Your documents")
-        pdf_docs = st.file_uploader(
-            "Upload your PDFs here and click on 'Process'", accept_multiple_files=True)
-        if st.button("Process"):
-            with st.spinner("Processing"):
-                # get pdf text
-                raw_text = get_pdf_text(pdf_docs)
+    # with st.sidebar:
+    #     st.subheader("Your documents")
+    #     pdf_docs = st.file_uploader(
+    #         "Upload your PDFs here and click on 'Process'", accept_multiple_files=True)
+    #     if st.button("Process"):
+    #         with st.spinner("Processing"):
+    #             # get pdf text
+    #             raw_text = get_pdf_text(pdf_docs)
 
-                # get the text chunks
-                text_chunks = get_text_chunks(raw_text)
+    #             # get the text chunks
+    #             text_chunks = get_text_chunks(raw_text)
 
-                # create vector store
-                vectorstore = get_vectorstore(text_chunks)
+    #             # create vector store
+    #             vectorstore = get_vectorstore(text_chunks)
                 
-                # create conversation chain
-                st.session_state.conversation = get_conversation_chain(vectorstore)
+    #             # create conversation chain
+    #             st.session_state.conversation = get_conversation_chain(vectorstore)
 
 
 if __name__ == '__main__':
